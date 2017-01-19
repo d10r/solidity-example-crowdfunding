7aa4b9e8b480d123b39c7bc02b84b82d5247b912
Mario Zupan <mzupanmz@gmail.com>
2017-01-19 19:51:52 +0100

    implement winner takes all crowdfunding contract

--- app.js
+++ app.js
@@ -12,11 +12,9 @@ var greeting = "HoHo Universe!";
 
 var greeterContract = web3.eth.contract(abi);
 
-console.log(web3.eth.estimateGas({data: code}));
-
-var greeter = greeterContract.new(greeting, {
+var greeter = greeterContract.new(1, 20, 100, {
     data: code,
-    gas: 300000,
+    gas: 900000,
 }, function(err, contract) {
     if (err) {
         console.error('there was an error: ' + err);
@@ -28,17 +26,33 @@ var greeter = greeterContract.new(greeting, {
     } else {
         console.log("Contract mined! Address: " + contract.address);
         console.log(contract);
+        // var events = contract.allEvents(function(error, log) {
+        //     if (!error)
+        //         console.log(log);
+        // });
     }
 });
 
-function greet() {
-    console.log(greeter.greet());
+function submit() {
+    greeter.submitProject.sendTransaction("yay", "whatever", {
+        from: web3.eth.accounts[Math.random() * 3 | 1],
+        value: 100,
+        gas: 600000,
+    });
 }
 
-function kill() {
-    console.log(greeter.kill());
+function support() {
+    greeter.supportProject.sendTransaction(web3.eth.accounts[Math.random() * 3 | 1], {
+        value: 100,
+        gas: 150000,
+    });
 }
 
-function isKilled() {
+function showprojects() {
+    console.log(greeter.projects());
+}
+
+function finish() {
+    console.log(greeter.finish());
     console.log(web3.eth.getCode(greeter.address));
 }
--- contract.sol
+++ contract.sol
@@ -1,14 +1,75 @@
 pragma solidity ^0.4.6;
 
-contract greeter {
-    address owner;
-    string greeting;
-    function kill() { if (msg.sender == owner) selfdestruct(owner); }
-    function greeter(string _greeting) public {
-        owner = msg.sender;
-        greeting = _greeting;
+contract WinnerTakesAll {
+    uint minimumEntryFee;
+    uint public deadlineProjects;
+    uint public deadlineCampaign;
+    uint public winningFunds;
+    address public winningAddress;
+    struct Project {
+        address addr;
+        string name;
+        string url;
+        uint funds;
+        bool initialized;
     }
-    function greet() constant returns (string) {
-        return greeting;
+    mapping (address => Project) projects;
+    address[] public projectAddresses;
+    event ProjectSubmitted(address addr, string name, string url, bool initialized);
+    event ProjectSupported(address addr, uint amount);
+    event PayedOutTo(address addr, uint winningFunds);
+
+    function WinnerTakesAll(uint _minimumEntryFee, uint _durationProjects, uint _durationCampaign) public {
+        if (_durationCampaign <= _durationProjects) {
+            throw;
+        }
+        minimumEntryFee = _minimumEntryFee;
+        deadlineProjects = now + _durationProjects* 1 seconds;
+        deadlineCampaign = now + _durationCampaign * 1 seconds;
+        winningAddress = msg.sender;
+        winningFunds = 0;
+    }
+    function submitProject(string name, string url) payable public returns (bool success) {
+        if (msg.value < minimumEntryFee) {
+            throw;
+        }
+        if (now > deadlineProjects) {
+            throw;
+        }
+        projects[msg.sender] = Project(msg.sender, name, url, 0, true);
+        projectAddresses.push(msg.sender);
+        ProjectSubmitted(msg.sender, name, url, projects[msg.sender].initialized);
+        return true;
+    }
+    function supportProject(address addr) payable public returns (bool success) {
+        if (msg.value <= 0) {
+            throw;
+        }
+        if (now > deadlineCampaign || now <= deadlineProjects) {
+            throw;
+        }
+        if (!projects[addr].initialized) {
+            throw;
+        }
+        projects[addr].funds += msg.value;
+        if (projects[addr].funds > winningFunds) {
+            winningAddress = addr;
+            winningFunds = projects[addr].funds;
+        }
+        ProjectSupported(addr, msg.value);
+        return true;
+    }
+    function getProjectInfo(address addr) public constant returns (string name, string url, uint funds) {
+        var project = projects[addr];
+        if (!project.initialized) {
+            throw;
+        }
+        return (project.name, project.url, project.funds);
+    }
+    function finish() {
+        if (now >= deadlineCampaign) {
+            PayedOutTo(winningAddress, winningFunds);
+            selfdestruct(winningAddress);
+        }
     }
 }
